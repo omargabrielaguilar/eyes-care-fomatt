1dbf7eba50c0128ee6020bd1de1bd14c3906d241
Omar <omareegab@gmail.com>
2025-12-26 16:33:51 -0500

    extracting change

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,16 +1,4 @@
-<!doctype html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <meta name="color-scheme" content="dark" />
-  <link rel="stylesheet" href="styles.css" />
-  @vite('resources/css/app.css', 'resources/js/app.js')
-  <title>PIXL</title>
-</head>
-
-<body class="bg-pixl-dark text-pixl-light flex gap-8 px-4 sm:h-dvh sm:overflow-clip xl:gap-16">
+<x-layout>
   <!-- Navigation -->
   @include('partials.navigation', ['showPostButton' => true])
 
@@ -535,6 +523,4 @@
 
   <!-- Sidebar -->
   @include('partials.aside')
-</body>
-
-</html>
+</x-layout>
